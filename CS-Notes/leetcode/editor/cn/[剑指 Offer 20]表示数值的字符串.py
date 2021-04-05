06167dd8e0c7ab27ee95e76825bb671d5737b8e4
chenyu02 <chenyu02@zhihu.com>
2021-04-05 22:51:51 +0800

    update

---
+++ CS-Notes/leetcode/editor/cn/[剑指 Offer 20]表示数值的字符串.py
@@ -0,0 +1,12 @@
+# 请实现一个函数用来判断字符串是否表示数值（包括整数和小数）。例如，字符串"+100"、"5e2"、"-123"、"3.1416"、"-1E-16"、"012
+# 3"都表示数值，但"12e"、"1a3.14"、"1.2.3"、"+-5"及"12e+5.4"都不是。 
+# 
+#  
+#  Related Topics 数学 
+#  👍 177 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def isNumber(self, s: str) -> bool:
+# leetcode submit region end(Prohibit modification and deletion)
